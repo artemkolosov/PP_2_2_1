b2c024c54115a87f850d9cd324a179ff839b5730
Kolosov <artemkol@mail.ru>
2024-02-07 11:12:07 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,20 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    //______________
+
+//    void addCar(Car car);
+//
+User getUserByCar(String model, int series);
+
+
+
+}
